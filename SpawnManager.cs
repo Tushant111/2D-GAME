8d9a01189dd2504269827e3841aac6b4eba5a55a
Tushant karihaloo <101183687+Tushant111@users.noreply.github.com>
2024-02-29 20:27:24 +0530

    Create SpawnManager.cs

---
+++ SpawnManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0,4);
+        {
+            Instantiate(animalPrefabs[animalIndex], new Vector3(Random.Range(-13,15) , 0, 20), animalPrefabs[animalIndex].transform.rotation);
+        }
+    }
+}
